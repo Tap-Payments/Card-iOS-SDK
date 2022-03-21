c5982ea16e0e00acc4d5f241bc9ff7401933f7ce
O Rabie <o.rabie@tap.company>
2022-03-21 12:37:23 +0200

    Added empty example project

---
+++ TapCardCheckOutKit/TapCardCheckoutExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapCardCheckoutExample
+//
+//  Created by Osama Rabie on 21/03/2022.
+//
+
+import UIKit
+import TapCardCheckOutKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

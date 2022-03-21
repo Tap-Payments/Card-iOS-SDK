e78d69429a5aac7c0920aafc2b9e9fdf12a48480
O Rabie <o.rabie@tap.company>
2022-03-21 12:33:46 +0200

    Add an empty framework project.

---
+++ TapCardCheckOutKit/TapCardCheckOutKit/TapCardCheckOutKit.h
@@ -0,0 +1,18 @@
+//
+//  TapCardCheckOutKit.h
+//  TapCardCheckOutKit
+//
+//  Created by Osama Rabie on 21/03/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapCardCheckOutKit.
+FOUNDATION_EXPORT double TapCardCheckOutKitVersionNumber;
+
+//! Project version string for TapCardCheckOutKit.
+FOUNDATION_EXPORT const unsigned char TapCardCheckOutKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapCardCheckOutKit/PublicHeader.h>
+
+
